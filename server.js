245118f707920210f0265d89cd8a5e3a31b62e7f
Kellin <kellinjoshua@gmail.com>
2019-05-15 19:44:21 -0700

    update server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
+var fs = require("fs");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes.js")(app, path);
+require("./app/routing/htmlRoutes.js")(app, path);
+
+app.listen(PORT, function () {
+    console.log("app listening on PORT: " + PORT);
+});
